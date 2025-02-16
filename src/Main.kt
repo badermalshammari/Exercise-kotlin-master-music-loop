1b8a27bc4b0211ada5aa279c43f57e16fdcb396f
Bader Alshammari <q8.badoori.11@gmail.com>
2025-02-16 22:41:01 +0300

    master- music loop

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,52 @@
+// Step 2: Crafting Your Song List
+val songs: MutableList<String> =  mutableListOf(
+    "Bohemian Rhapsody",
+    "Hotel California",
+    "Imagine",
+    "Shape of You",
+    "Billie Jean")
+// Step 3: Exploring Loop Magic
+fun playAll(songs: List<String>) {
+    for (song in songs) {
+        println("Now playing $song")
+    }
+}
+// Step 4: Mastering the Playlist
+fun addSong(song: String) {
+    songs.add(song)
+}
+// Step 5: Feeling Shuffle-y?
+fun shuffleSongs() {
+    songs.shuffle()
+}
+//Step 6: Get Creative with Your Music!
+fun removeSong(song: String) {
+    songs.removeAt(songs.indexOf(song))
+}
+
+//fun findByArtist(artistName: String) {
+//
+//    println("Searching for songs by: $artistName ")
+//}
+
+// need some changes for the code that the upper steps didn't ask us to do
+
+fun showPlaylist(songs: List<String>) {
+    println("Your Playlist:")
+    for (song in songs) {
+        println("- $song")
+    }
+}
+
 fun main() {
-    println("Hello World!")
+    playAll(songs)
+    addSong("Smells Like Teen Spirit")
+    println("////////////////////////////")
+    shuffleSongs()
+    playAll(songs)
+    println("////////////////////////////")
+    removeSong("Smells Like Teen Spirit")
+    playAll(songs)
+    showPlaylist(songs)
+
 }
